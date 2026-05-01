8c1e0f4f7e128f266067190f8be356b0baaf56be
Kutuaz <renatoortiz2003@gmail.com>
2026-05-01 07:28:26 -0400

    configuracion inicial arquitectura MVC y conexion Azure SQL

---
+++ WebHotelPacificRef/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebHotelPacificRef.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
